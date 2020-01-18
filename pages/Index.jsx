29fdc05baefb671bf90e4be64eab3c5083d56528
“Eduardo <eduardo.pineda@gorillalogic.com>
2020-01-18 16:06:13 -0600

    Feature complete

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
-import { Box, Main } from 'grommet';
+import {
+  Container,
+  Typography,
+  Box,
+  Grid,
+  CssBaseline
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Header from '../components/Header';
+import PostCard from '../components/PostCard';
 import blogPosts from '../data/blog-posts';
+import Footer from '../components/Footer';
 
-const AppBar = props => (
-  <Box
-    tag="header"
-    direction="row"
-    align="center"
-    justify="between"
-    background="brand"
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation="medium"
-    style={{ zIndex: '1' }}
-    {...props}
-  />
-);
+const useStyles = makeStyles({
+  container: {
+    minHeight: '80vh'
+  }
+});
 
-const Index = () => (
-  <>
-    <AppBar>
-      <p>My Blog in Next.js</p>
-    </AppBar>
-    <Main pad="large">
-      {blogPosts.map((post, index) => (
-        <div key={index}>{post.title}</div>
-      ))}
-    </Main>
-  </>
-);
+const Index = () => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Header />
+      <Container maxWidth="lg" className={classes.container}>
+        <Box my={4} display="flex" justifyContent="center">
+          <Typography variant="h4" component="h1" gutterBottom>
+            Featured Blog posts
+          </Typography>
+        </Box>
+        <Grid container spacing={4}>
+          {blogPosts.map(post => (
+            <PostCard key={post.title} post={post} />
+          ))}
+        </Grid>
+      </Container>
+      <Footer title="My Blog" description="Hi there, this is my blog!" />
+    </React.Fragment>
+  );
+};
 
 export default Index;
